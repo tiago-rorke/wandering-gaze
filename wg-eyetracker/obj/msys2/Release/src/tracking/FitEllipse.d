ae7209c68fef5f799c35d072fa774b3883dfd113
tiago-rorke <hello@tiago.nz>
2019-05-18 19:26:55 +0100

    clean dirs and update gitignore

--- wg-eyetracker/obj/msys2/Release/src/tracking/FitEllipse.d
+++
@@ -1,5 +0,0 @@
-obj/msys2/Release/src/tracking/FitEllipse.o: \
- C:/msys64/of_v0.9.8_msys2_release/apps/myApps/wg-eyetracker_02/src/tracking/FitEllipse.cpp \
- C:/msys64/of_v0.9.8_msys2_release/apps/myApps/wg-eyetracker_02/src/tracking/FitEllipse.h
-
-C:/msys64/of_v0.9.8_msys2_release/apps/myApps/wg-eyetracker_02/src/tracking/FitEllipse.h:
